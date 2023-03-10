cc60797e2de760f6fcc694ecda259fbd1837b680
Oleksandr Tkachuk <oleksandr.tkachuk@luxinten.com>
2023-03-10 13:18:40 +0200

    Section 3: React Fundamentals / 27. JSX - Javascript

--- src/index.js
+++ src/index.js
@@ -9,30 +9,21 @@ const BookList = () => {
         <Book/>
         <Book/>
         <Book/>
+        <Book/>
     </section>
 }
 
+const author = 'Alice Schertle';
+
 const Book = () => {
+    const title = 'Little Blue Truck\'s Valentine';
     return <article className='book'>
-        <Image/>
-        <Title/>
-        <Author/>
+        <img src='./images/book-1.jpg' alt='Little Blue Trucks Valentine'/>
+        <h2>{title}</h2>
+        <h4>{author}</h4>
     </article>
 }
 
-
-const Image = () => {
-    return <img src='./images/book-1.jpg' alt='Little Blue Trucks Valentine'/>
-};
-const Title = () => <h2>Little Blue Truck's Valentine</h2>;
-const Author = () => {
-    const inlineHeadingStyles = {
-        color: '#617d98',
-        fontSize: '0.75rem',
-        marginTop: '0.5rem'
-    };
-    return <h4 style={inlineHeadingStyles}>Alice Schertle</h4>;
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
