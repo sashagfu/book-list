38db87c0c35850a438735cfbd5a5bfbb22ddb8c8
Oleksandr Tkachuk <oleksandr.tkachuk@luxinten.com>
2023-03-10 13:49:02 +0200

    Section 3: React Fundamentals / 30. Props - Multiple Approaches

--- src/index.js
+++ src/index.js
@@ -3,25 +3,43 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-const author = 'Alice Schertle';
-const title = 'Little Blue Truck\'s Valentine';
-const img ='./images/book-1.jpg';
+const firstBook = {
+    author: 'Alice Schertle',
+    title: 'Little Blue Truck\'s Valentine',
+    img: './images/book-1.jpg'
+};
+const secondBook = {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg'
+};
+
+// const author = 'Alice Schertle';
+// const title = 'Little Blue Truck\'s Valentine';
+// const img ='./images/book-1.jpg';
 
 const BookList = () => {
     return <section className='book-list'>
-        <Book author={author} title={title} img={img}/>
-        <Book author={author} title={title} img={img}/>
-        <Book author={author} title={title} img={img}/>
-        <Book author={author} title={title} img={img}/>
+        <Book
+            author={firstBook.author}
+            title={firstBook.title}
+            img={firstBook.img}
+        />
+        <Book
+            author={secondBook.author}
+            title={secondBook.title}
+            img={secondBook.img}
+        />
     </section>
 }
 
 const Book = (props) => {
     console.log(props);
+    const {img, title, author} = props;
     return <article className='book'>
-        <img src={props.img} alt={props.title}/>
-        <h2>{props.title}</h2>
-        <h4>{props.author}</h4>
+        <img src={img} alt={title}/>
+        <h2>{title}</h2>
+        <h4>{author}</h4>
     </article>
 }
 
