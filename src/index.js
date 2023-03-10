8f294e9e0447ad0ba3960b8ba60cfa0623d8b3d1
Oleksandr Tkachuk <oleksandr.tkachuk@luxinten.com>
2023-03-10 15:25:49 +0200

    Section 3: React Fundamentals / 35. Object as a Prop

--- src/index.js
+++ src/index.js
@@ -3,60 +3,34 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-const firstBook = {
+const books = [{
+    id: 1,
     author: 'Alice Schertle',
     title: 'Little Blue Truck\'s Valentine',
     img: './images/book-1.jpg'
-};
-const secondBook = {
+}, {
+    id: 2,
     author: 'James Clear',
     title: 'Atomic Habits',
     img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg'
-};
-
-// const author = 'Alice Schertle';
-// const title = 'Little Blue Truck\'s Valentine';
-// const img ='./images/book-1.jpg';
+}];
 
 const BookList = () => {
     return <section className='book-list'>
-        <Book
-            author={firstBook.author}
-            title={firstBook.title}
-            img={firstBook.img}
-        >
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <button>Click me</button>
-        </Book>
-        <Book
-            author={secondBook.author}
-            title={secondBook.title}
-            img={secondBook.img}
-        />
-        <Book
-            author={secondBook.author}
-            title={secondBook.title}
-            img={secondBook.img}
-        />
-        <Book
-            author={secondBook.author}
-            title={secondBook.title}
-            img={secondBook.img}
-        />
+        {books.map((book) => {
+            return <Book {...book} key={book.id}/>;
+        })}
     </section>
 }
 
 const Book = (props) => {
-    console.log(props);
-    const {img, title, author, children} = props;
+    const {img, title, author} = props;
     return <article className='book'>
         <img src={img} alt={title}/>
         <h2>{title}</h2>
         <h4>{author}</h4>
-        {children}
     </article>
 }
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
