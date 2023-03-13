b40760ae526d95cdc46b69a89c38efb0e9bcf40a
Oleksandr Tkachuk <oleksandr.tkachuk@luxinten.com>
2023-03-13 17:41:30 +0200

    Section 3: React Fundamentals / 46. Local Images (src folder)

---
+++ src/books.js
@@ -0,0 +1,24 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+
+export const books = [
+    {
+        id: 1,
+        author: 'Alice Schertle',
+        title: 'Little Blue Truck\'s Valentine',
+        img: img1
+    },
+    {
+        id: 2,
+        author: 'James Clear',
+        title: 'Atomic Habits',
+        img: img2
+    },
+    {
+        id: 3,
+        author: 'Ron DeSantis',
+        title: 'The Courage to Be Free',
+        img: img3
+    }
+];
--- src/index.js
+++ src/index.js
@@ -2,55 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
-
-const books = [{
-    id: 1,
-    author: 'Alice Schertle',
-    title: 'Little Blue Truck\'s Valentine',
-    img: './images/book-1.jpg'
-}, {
-    id: 2,
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg'
-}];
+import {books} from './books';
+import Book from './Book';
 
 const BookList = () => {
     return <section className='book-list'>
-        <EventExamples/>
         {books.map((book) => {
             return <Book {...book} key={book.id}/>;
         })}
     </section>
-}
-
-const EventExamples = () => {
-    return (
-        <section>
-            <form>
-                <h2>Typical Form</h2>
-                <input
-                    type='text'
-                    name='product'
-                    onChange={(e) => console.log(e.target.value)}
-                    style={{margin: '1rem 0'}}
-                />
-                <button type='submit'>submit</button>
-                <div>
-                    <button type='button' onClick={() => console.log('test click')}>click me</button>
-                </div>
-            </form>
-        </section>
-    );
-}
-
-const Book = (props) => {
-    const {img, title, author} = props;
-    return <article className='book'>
-        <img src={img} alt={title}/>
-        <h2>{title}</h2>
-        <h4>{author}</h4>
-    </article>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
